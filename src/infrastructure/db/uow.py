deb62b20ab5190cef0ab92c369cb27d58a9c13aa
Albert Mezhakov <albertmezhakov@gmail.com>
2025-06-09 02:40:07 +0300

    Реализован слой инфраструктуры - models, session, UserRepository, UnitOfWork

---
+++ src/infrastructure/db/uow.py
@@ -0,0 +1,25 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from infrastructure.db.repositories import UserRepository
+
+
+class UnitOfWork:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+        self.users = UserRepository(self.session)
+
+    async def __aenter__(self) -> "UnitOfWork":
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb)-> None:
+        if exc_type:
+            await self.session.rollback()
+        else:
+            await self.session.commit()
+        await self.session.close()
+
+    async def commit(self)-> None:
+        await self.session.commit()
+
+    async def rollback(self)-> None:
+        await self.session.rollback()
