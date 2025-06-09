ec22a08efcecd25ba8437cba62649fbcdc03552e
Albert Mezhakov <albertmezhakov@gmail.com>
2025-06-09 19:09:06 +0300

    feat(redis): добавлена поддержка Redis

---
+++ src/infrastructure/redis/client.py
@@ -0,0 +1,10 @@
+import redis.asyncio as redis
+from config import settings
+
+redis_client = redis.Redis(
+    host=settings.fsm_redis_host,
+    port=settings.fsm_redis_port,
+    db=settings.fsm_redis_db,
+    password=settings.fsm_redis_pass,
+    decode_responses=True,
+)
