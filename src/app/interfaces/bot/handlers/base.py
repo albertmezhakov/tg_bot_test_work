913679c0e02c63f93d9181f54fc05d563d601089
Albert Mezhakov <albertmezhakov@gmail.com>
2025-06-10 23:58:38 +0300

    feat(bot): команда creator

---
+++ src/app/interfaces/bot/handlers/base.py
@@ -0,0 +1,19 @@
+from aiogram import Router
+from aiogram.enums import ParseMode
+from aiogram.filters import Command
+from aiogram.types import Message
+
+from config import settings
+
+router = Router()
+
+
+@router.message(Command(commands=["ping", "health", "healthcheck"]))
+async def ping_pong(message: Message):
+    await message.answer("200 pong")
+
+
+@router.message(Command(commands=["creator"]))
+async def creator(message: Message):
+    if message.from_user.id == settings.creator_id:
+        await message.answer("*Master?*", parse_mode=ParseMode.MARKDOWN)
