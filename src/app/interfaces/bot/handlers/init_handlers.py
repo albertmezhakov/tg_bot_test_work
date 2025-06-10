1c84c0825c9a86fa323a22d70c22d0798982e548
Albert Mezhakov <albertmezhakov@gmail.com>
2025-06-10 21:35:16 +0300

    feat(bot): обработка блокировки бота

--- src/app/interfaces/bot/handlers/init_handlers.py
+++ src/app/interfaces/bot/handlers/init_handlers.py
@@ -1,11 +1,12 @@
 from aiogram import Router
 
-from . import fallback, profile, ping_pong
+from . import fallback, profile, ping_pong, errors
 
 
 def setup_handlers() -> Router:
     router = Router()
     router.include_router(profile.router)
     router.include_router(ping_pong.router)
+    router.include_router(errors.router)
     router.include_router(fallback.router)
     return router
