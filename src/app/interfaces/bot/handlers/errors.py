14e97979e1057acd167e13d38a1d7c2c6b54d9e3
Albert Mezhakov <albertmezhakov@gmail.com>
2025-06-10 21:35:16 +0300

    Добавлено обработка блокировки бота

---
+++ src/app/interfaces/bot/handlers/errors.py
@@ -0,0 +1,19 @@
+from aiogram import Router
+from aiogram.exceptions import TelegramBadRequest
+from aiogram.types import ErrorEvent, ChatMemberUpdated
+from loguru import logger
+
+router = Router()
+
+
+@router.errors()
+async def bot_blocked_handler(event: ErrorEvent) -> None:
+    exception = event.exception
+    if isinstance(exception, TelegramBadRequest) and "bot was blocked by the user" in str(exception).lower():
+        logger.info(event.message.from_user.id)
+        logger.info(exception)
+
+
+@router.my_chat_member()
+async def handle_my_chat_member(event: ChatMemberUpdated):
+    pass
