29e03d3446cd389dcbe3f48049c810e884b76f45
Albert Mezhakov <albertmezhakov@gmail.com>
2025-06-10 18:50:11 +0300

    feat(bot): команда ping pong

--- src/app/interfaces/bot/handlers/init_handlers.py
+++ src/app/interfaces/bot/handlers/init_handlers.py
@@ -1,9 +1,10 @@
 from aiogram import Router
-from . import fallback, profile
+from . import fallback, profile, ping_pong
 
 
 def setup_handlers() -> Router:
     router = Router()
     router.include_router(profile.router)
+    router.include_router(ping_pong.router)
     router.include_router(fallback.router)
     return router
