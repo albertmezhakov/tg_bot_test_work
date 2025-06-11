a5fdf584020f69cdc00f062d755f69382ad2abda
Albert Mezhakov <albertmezhakov@gmail.com>
2025-06-12 00:00:43 +0300

    Добавлены абстракции для соблюдения SOLID

--- src/app/interfaces/bot/middlewares/dependency_injection.py
+++ src/app/interfaces/bot/middlewares/dependency_injection.py
@@ -1,5 +1,6 @@
 from aiogram import BaseMiddleware
 
+from app.domain.uow import AbstractUnitOfWork
 from app.use_cases.user_game_service import UserGameService
 from app.use_cases.user_profile_service import UserProfileService
 from infrastructure.db.session import AsyncSessionLocal
@@ -9,7 +10,7 @@ from infrastructure.db.uow import UnitOfWork
 class DependencyInjectionMiddleware(BaseMiddleware):
     async def __call__(self, handler, event, data):
         async with AsyncSessionLocal() as session:
-            uow = UnitOfWork(session)
+            uow: AbstractUnitOfWork = UnitOfWork(session)
             data["user_profile_service"] = UserProfileService(uow)
             data["user_game_service"] = UserGameService(uow)
             return await handler(event, data)
