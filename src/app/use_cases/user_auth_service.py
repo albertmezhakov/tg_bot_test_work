667c101d14ca2092d1fdb5c709ec69f22a352c1f
Albert Mezhakov <albertmezhakov@gmail.com>
2025-06-11 23:22:16 +0300

    Подправил Domain Layer и фикс мелких багов

--- src/app/use_cases/user_auth_service.py
+++ src/app/use_cases/user_auth_service.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timezone
 
+from app.domain.auth_result import AuthResult, AuthStatus
 from app.domain.user import User
 from config import settings
 from infrastructure.db.uow import UnitOfWork
@@ -10,21 +11,22 @@ class UserAuthService:
         self.uow = uow
 
     async def check_or_register(
-        self, user_id: int, username: str | None, passphrase: str | None
-    ) -> tuple[bool, str | None]:
+            self, user_id: int, username: str | None, passphrase: str | None
+    ) -> AuthResult:
         async with self.uow:
             user_exists = await self.uow.users.exists_by_social_id(user_id)
+            print(user_exists)
             if user_exists:
-                return True, None
+                return AuthResult(AuthStatus.SUCCESS)
             if settings.register_passphrase and passphrase != settings.register_passphrase:
-                return False, "Enter passphrase for register in bot:"
+                return AuthResult(AuthStatus.NEED_PASSPHRASE, "Enter passphrase for register in bot:")
             if not username:
-                return False, (
-                    "Для регистрации заполните, пожалуйста, Имя пользователя в своем профиле, "
-                    "иначе вас не смогут найти другие участники!"
+                return AuthResult(
+                    AuthStatus.NEED_USERNAME,
+                    "Для регистрации заполните, пожалуйста, Имя пользователя в своем профиле, иначе вас не смогут найти другие участники!"
                 )
             await self.uow.users.create(
-                User(social_id=user_id, username=username, registration_date=datetime.now(timezone.utc))
+                User(social_id=user_id, username=username, registration_date=datetime.now())
             )
             await self.uow.commit()
-            return False, "Welcome to bot"
+            return AuthResult(AuthStatus.REGISTERED, "Welcome to bot")
